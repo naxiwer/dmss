99d746089deb9095c3454296ce588b990abbf131
liguopeng1 <liguopeng1@9fbank.cc>
2020-07-29 18:35:53 +0800

    0.1.0.4

--- dmss/urls.py
+++ dmss/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 admin.autodiscover()
-from login import views as homeviews
+#from login import views as homeviews
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
